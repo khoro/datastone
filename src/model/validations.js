80c4c757f426a7f27ad1b7fe8519d7091595c837
Ali Davut <adavut@gmail.com>
2019-03-28 01:23:18 +0300

    implement validates and isValid

--- src/model/validations.js
+++ src/model/validations.js
@@ -19,9 +19,35 @@ export default Klass => {
       validators[name] = validator;
     }
 
+    static validates(field, validations) {
+      this.validations.push({ field, validations });
+    }
+
     get errors() {
       if(!this.__errors) this.__errors = new Errors();
       return this.__errors;
+    }
+
+    get isValid() {
+      this.__errors = new Errors();
+
+      return new Promise(async (resolve) => {
+        for(let item of this.constructor.validations) {
+          for(let validatorName in item.validations) {
+            const ValidatorClass = this.constructor.validators[validatorName];
+            const validator = new ValidatorClass(
+              this,
+              item.field,
+              this[item.field],
+              item.validations[validatorName]
+            );
+
+            await validator.validate();
+          }
+        }
+
+        resolve(!Object.keys(this.errors.messages).length);
+      });
     }
   }
 }
--- test/model/validations.spec.js
+++ test/model/validations.spec.js
@@ -1,4 +1,5 @@
 import validations from '@model/validations';
+import Validator from '@validators/validator';
 
 describe('src/model/validations', () => {
   describe('inheritance', () => {
@@ -19,9 +20,66 @@ describe('src/model/validations', () => {
 
       Model.registerValidator(MyValidator);
       Model.registerValidator(AnotherCoolValidator);
-      
+
       expect(Model.validators['my']).toBe(MyValidator);
       expect(Model.validators['anotherCool']).toBe(AnotherCoolValidator);
+    });
+  });
+
+  describe('#validates', () => {
+    it('adds the validations correctly', () => {
+      const Model = validations(class {});
+      class MyModel extends Model {};
+      MyModel.validates('field1', { val1: true });
+      MyModel.validates('field2', { val2: { inner: true } });
+      expect(MyModel.validations).toEqual([
+        { field: 'field1', validations: { val1: true } },
+        { field: 'field2', validations: { val2: { inner: true } } }
+      ]);
+    });
+  });
+
+  describe('#isValid', () => {
+    it('runs all the validations sync', async () => {
+      const cb1 = jest.fn();
+      const cb2 = jest.fn();
+      const results = [];
+
+      class MockValidator extends Validator {
+        async validate() {
+          await new Promise(resolve => {
+            setTimeout(() => {
+              cb1(this);
+              results.push(1);
+              resolve();
+            }, 100);
+          });
+        }
+      }
+
+      class AnotherMockValidator extends Validator {
+        validate() {
+          results.push(2);
+          cb2(this);
+        }
+      }
+
+      const Model = validations(class {});
+      Model.registerValidator(MockValidator);
+      Model.registerValidator(AnotherMockValidator);
+      class MyModel extends Model {};
+      MyModel.validates('name', { mock: true });
+      MyModel.validates('name', { anotherMock: true });
+      const record = new MyModel();
+      record.name = 'say my name';
+
+      expect(await record.isValid).toBe(true);
+      expect(results).toEqual([1, 2]);
+      expect(cb1.mock.calls[0][0].field).toEqual('name');
+      expect(cb1.mock.calls[0][0].model).toEqual(MyModel);
+      expect(cb1.mock.calls[0][0].record).toEqual(record);
+      expect(cb1.mock.calls[0][0].rule).toEqual(true);
+      expect(cb1.mock.calls[0][0].value).toEqual('say my name');
     });
   });
 });
