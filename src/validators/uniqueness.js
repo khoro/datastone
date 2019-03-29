226384a4008c9beb108875b4f1e96a9e9135c952
Ali Davut <adavut@gmail.com>
2019-03-30 00:49:40 +0300

    add uniqueness validator scope

--- src/validators/uniqueness.js
+++ src/validators/uniqueness.js
@@ -1,10 +1,24 @@
 import Validator from './validator';
+import _ from 'lodash';
 
 export default class UniquenessValidator extends Validator {
   static message = 'has been taken';
 
   async validate() {
-    if(this.rule && this.value && await this.model.where({ [this.field]: this.value }).first) {
+    if (!this.rule) return;
+    const query = { [this.field]: this.value || null };
+
+    if(this.rule.scope) {
+      if(_.isArray(this.rule.scope)) {
+        this.rule.scope.forEach(field => {
+          query[field] = this.record[field] || null;
+        });
+      } else {
+        query[this.rule.scope] = this.record[this.rule.scope] || null;
+      }
+    }
+
+    if(await this.model.where(query).first) {
       this.record.errors.add(this.field, this.message);
     }
   }
--- test/validators/uniqueness.spec.js
+++ test/validators/uniqueness.spec.js
@@ -25,7 +25,61 @@ describe('src/validators/unqiueness', () => {
   });
 
   it('adds error if record exists', async () => {
-    await new UniquenessValidator(record, 'email', 'mail1@mail.com', true).validate();
+    await new UniquenessValidator(
+      record,
+      'email',
+      'mail1@mail.com',
+      true
+    ).validate();
+
+    expect(record.errors.messages.email).toEqual([UniquenessValidator.message]);
+  });
+
+  it('checks multiple scopes if provided', async () => {
+    record.name = 'name2';
+
+    await new UniquenessValidator(
+      record,
+      'email',
+      'mail1@mail.com',
+      { scope: 'name' }
+    ).validate();
+
+    expect(record.errors.messages.email).toEqual(undefined);
+
+    record.name = 'name1';
+    await new UniquenessValidator(
+      record,
+      'email',
+      'mail1@mail.com',
+      { scope: 'name' }
+    ).validate();
+
+    expect(record.errors.messages.email).toEqual([UniquenessValidator.message]);
+
+    record = new User();
+    record.name = 'name1';
+
+    await new UniquenessValidator(
+      record,
+      'email',
+      'mail1@mail.com',
+      { scope: ['name', 'password'] }
+    ).validate();
+
+    expect(record.errors.messages.email).toEqual(undefined);
+
+    record = new User();
+    record.name = 'name1';
+    record.password = 'pass1';
+
+    await new UniquenessValidator(
+      record,
+      'email',
+      'mail1@mail.com',
+      { scope: ['name', 'password'] }
+    ).validate();
+
     expect(record.errors.messages.email).toEqual([UniquenessValidator.message]);
   });
 })
